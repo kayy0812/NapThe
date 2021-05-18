b6daf1591e7093c28c10273a5636c3c1e066dfe9
kayy0812 <30899127+kayy0812@users.noreply.github.com>
2021-05-18 16:52:17 +0700

    Update EventListener.php

--- src/KaYuuVN/NapThe/EventListener.php
+++ src/KaYuuVN/NapThe/EventListener.php
@@ -4,15 +4,14 @@ namespace KaYuuVN\NapThe;
 use pocketmine\event\Listener;
 use pocketmine\Player;
 use pocketmine\event\player\{
-	PlayerJoinEvent
+    PlayerJoinEvent
 };
 
 /**
  * Class EventListener
  * @package KaYuuVN\NapThe
  */
-Class EventListener implements Listener
-{
+class EventListener implements Listener{
     
     /** @var NapThe */
     private $plugin;
@@ -21,12 +20,9 @@ Class EventListener implements Listener
     * EventListener constructor.
     * @param NapThe $plugin
     */
-    public function __construct(NapThe $plugin)
-    {
-		$this->plugin = $plugin;
-	}
+    public function __construct(NapThe $plugin) {
+	$this->plugin = $plugin;
+    }
 
-	public function onJoin(PlayerJoinEvent $event)
-	{
-	}
+    public function onJoin(PlayerJoinEvent $event) {}
 }
