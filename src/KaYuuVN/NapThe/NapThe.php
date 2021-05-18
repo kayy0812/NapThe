49413e4a25baa4768fb7edea772de61f9c0c5c35
kayy0812 <30899127+kayy0812@users.noreply.github.com>
2021-05-18 16:45:18 +0700

    Update NapThe.php

--- src/KaYuuVN/NapThe/NapThe.php
+++ src/KaYuuVN/NapThe/NapThe.php
@@ -11,16 +11,9 @@ use pocketmine\utils\Config;
 use pocketmine\utils\TextFormat as TF;
 use KaYuuVN\NapThe\Commands\NapTheCommand;
 
-class NapThe extends PluginBase
-{
+class NapThe extends PluginBase {
 	public $config = [];
-	public $dropdown_list_type = [
-    	"Viettel",
-    	"Mobiphone",
-    	"Vinaphone",
-    	"Zing",
-    	"Gate"
-    ];
+	public $dropdown_list_name = [];
     public $dropdown_list_amount = [
     	"10000",
     	"20000",
@@ -31,8 +24,7 @@ class NapThe extends PluginBase
     	"1000000"
     ];
 
-	public function onEnable(): void 
-	{
+	public function onEnable(): void {
 		$this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
 		$this->getServer()->getCommandMap()->register("napthe", new NapTheCommand($this));
 		$files = array("setting.yml");
@@ -44,25 +36,32 @@ class NapThe extends PluginBase
 		}
 		$this->config = new Config($this->getDataFolder(). "setting.yml", Config::YAML);
 		$this->token = $this->getServer()->getPluginManager()->getPlugin('PointAPI');
+		$this->dropdown_list_name = $this->Status();
 	}
 
-	public function Replace(string $subject, $replace = []): string
-	{
+	public function Replace(string $subject, $replace = []): string {
 		foreach ($replace as $var => $code) {
 			$subject = str_replace("{".$var."}", $code, $subject);
 		}
 		return $subject;
 	}
 
-	public function CallStatus($id)
-	{
+	public function Status($id = null) {
 		$json = file_get_contents('http://api.napthengay.com/Status.php');
 		$decode = json_decode($json, true);
+		if (empty($id)) {
+			$names = array();
+			$id = 0;
+			foreach ($decode as $val) {
+				$id += 1;
+				$names[$id] = $val['name'];
+			}
+			return $names;
+		}
 		foreach ($decode as $data) {
 			if ($data['id'] == $id) {
-				return ($data['status'] == 1) ? "Hoạt Động" : "Bảo Trì";
+				return ($data['status'] == 1) ? "§r§2Hoạt Động§r" : "§r§cBảo Trì§r";
 			}
 		}
 	}
-}
-		
+}		
