d09379341d0463f0c5d4993cd2617444332aaa84
kayy0812 <30899127+kayy0812@users.noreply.github.com>
2021-05-18 16:54:39 +0700

    Optimize data transmission

--- src/KaYuuVN/NapThe/Servers/NapTheNgay.php
+++ src/KaYuuVN/NapThe/Servers/NapTheNgay.php
@@ -8,10 +8,20 @@ use KaYuuVN\NapThe\{
 	Form
 };
 
-Class NapTheNgay
-{
-	public function __construct(NapThe $plugin, Player $player, int $type, $seri, $pin, $amount)
-    {
+class NapTheNgay {
+
+    private $type;
+    private $seri;
+    private $pin;
+    private $amount;
+
+    private $api_url;
+    private $merchant_id;
+    private $email;
+    private $secure_code;
+    private $trans_id;
+
+    public function __construct(NapThe $plugin, Player $player, int $type, $seri, $pin, $amount) {
         $this->plugin = $plugin;
         $this->player = $player;
 
@@ -21,25 +31,24 @@ Class NapTheNgay
         $this->amount = $amount;
 
         //Du lieu lay tu config setting.yml
-        $this->api_url = $this->plugin->config->get('api-url');
-        $this->merchant_id = $this->plugin->config->get('merchant-id');
-        $this->email = $this->plugin->config->get('email');
-        $this->secure_code = $this->plugin->config->get('secure-code');
+        $this->api_url = $this->plugin->config->get('api_url');
+        $this->merchant_id = $this->plugin->config->get('merchant_id');
+        $this->mail = $this->plugin->config->get('mail');
+        $this->secure_code = $this->plugin->config->get('secure_code');
         $this->trans_id = time();
 
     }
 
-    public function CallToHost()
-    {
+    public function send() {
     	$arrayPost = array(
-	        'merchant_id'=>intval($this->merchant_id),
-	        'api_email'=>trim($this->email),
-	        'trans_id'=>trim($this->trans_id),
-	        'card_id'=>trim($this->type),
-	        'card_value'=> intval($this->amount),
-	        'pin_field'=>trim($this->pin),
-	        'seri_field'=>trim($this->seri),
-	        'algo_mode'=>'hmac'
+	        'merchant_id' => intval($this->merchant_id),
+	        'api_email' => trim($this->mail),
+	        'trans_id' => trim($this->trans_id),
+	        'card_id' => trim($this->type),
+	        'card_value' => intval($this->amount),
+	        'pin_field' => trim($this->pin),
+	        'seri_field' => trim($this->seri),
+	        'algo_mode' => 'hmac'
         );
         $data_sign = hash_hmac('SHA1', implode('',$arrayPost), $this->secure_code);
         $arrayPost['data_sign'] = $data_sign;
@@ -55,42 +64,34 @@ Class NapTheNgay
 			CURLOPT_POSTFIELDS=>http_build_query($arrayPost)
 		));
 		$data = curl_exec($curl);
+		$status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+		$result = json_decode($data, true);
 
-		$this->status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-		$this->result = json_decode($data, true);
-		$this->sendCallback();
-
-		
-    }
-
-    public function sendCallback()
-    {
-    	$form = new Form($this->plugin, $this->player);
-    	if ($this->status == 200) {
-    		$amount = $this->result['amount'];
-			switch ($this->type) {
-				case 1: $type = "Viettel"; break;
-				case 2: $type = "Mobiphone"; break;
-				case 3: $type = "Vinaphone"; break;
-				case 4: $type = "Zing"; break;
-				case 5: $type = "Gate"; break;
-				case 6: $type = "VCoin"; break;
-			}
-			if ($this->result['code'] == 100) {
+		$form = new Form($this->plugin, $this->player);
+    	if ($status == 200) {
+    		$amount = $result['amount'];
+    		foreach ($this->plugin->dropdown_list_name as $id => $val) {
+    			if ($this->type == $id) {
+    				$type = $val;
+    				break;
+    			}
+    		}
+			if ($result['code'] == 100) {
 				$form->NapThe($this->plugin->Replace($this->plugin->config->get('accept_callback'), [
 					"TYPE" => $type,
 					"AMOUNT" => $amount
 				]));
-				$this->plugin->token->addPoint($this->player, $this->plugin->config->get('amount_callback')[$amount] * $this->plugin->config->get('multip'));
+				$multip = ($this->plugin->config->get('multip') == 0) ? 1 : $this->plugin->config->get('multip');
+				$this->plugin->token->addMoney($this->player, $this->plugin->config->get('amount_callback')[$amount] * $multip);
 	    	} else {
 				$form->NapThe($this->plugin->Replace($this->plugin->config->get('deny_callback'), [
-					"REASON" => $this->result['msg'],
-					"CODE" => $this->result['code']
+					"REASON" => $result['msg'],
+					"CODE" => $result['code']
 				]));
 			}
 		} else { 
 			$form->NapThe($this->plugin->Replace($this->plugin->config->get('error_host_callback'), [
-					"CODE" => $this->status,
+					"CODE" => $status,
 				])); 
 		}
     }
